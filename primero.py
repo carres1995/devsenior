acc11ec0ab6d2d5773751a0dc790727705347896
Carlos <carlosres1995@gmail.com>
2025-01-24 16:41:40 -0500

    agregue 2 archivos primero y tuplas.py

---
+++ primero.py
@@ -0,0 +1,10 @@
+a= 5
+b= 6
+c= 3
+resultado = (b > a) and (c < a) or (5>12)
+print(resultado)
+
+
+
+
+
---
+++ tuplas.py
@@ -0,0 +1,6 @@
+#marcas =('mazda', 'renault', 'fiat', 'bmw', 'chevrolet', 'renault')
+#marcas2 = tuple(('mazda', 'renault', 'fiat', 'bmw', 'chevrolet', 'renault'))
+#print(marcas)
+#print(marcas.count("renault"))
+#print(marcas.index("renault"))
+
